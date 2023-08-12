e4360416dd35db46a7dd9b37ac4efb9bb182508f
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-08-12 13:06:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ function start() {
       toolbox: document.getElementById('toolbox-categories'),
       renderer: 'zelos',
       CORNER_RADIUS: 20,
+      zoom:
+         {controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2,
+          pinch: true},
+     trashcan: true
     });
 
     
