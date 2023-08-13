3dd814eb52606a22879dac83a2f0082c5bf54195
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-08-13 22:28:05 +0200

    Update base_main_constructor.js

--- blocks/b4d/base_main_constructor.js
+++ blocks/b4d/base_main_constructor.js
@@ -10,16 +10,15 @@ const blockName = "base_main_constructor";
       "name": "NAME"
     },
     {
-      "type": "field_input",
-      "name": "TOKEN",
-      "text": "token"
+      "type": "input_value",
+      "name": "TOKEN"
     }
   ],
   "nextStatement": null,
   "colour": '#F4B400',
   "tooltip": "",
   "helpUrl": ""
-};
+ };
 Blockly.Blocks[blockName] = {
     init: function() {
         this.jsonInit(blockData);
@@ -28,8 +27,8 @@ Blockly.Blocks[blockName] = {
 
 javascript.javascriptGenerator.forBlock['base_main_constructor'] = function(block, generator) {
   var statements_name = generator.statementToCode(block, 'NAME');
-  var text_token = block.getFieldValue('TOKEN');
+  var value_token = generator.valueToCode(block, 'TOKEN', javascript.Order.ATOMIC);
   // TODO: Assemble javascript into code variable.
-  var code = 'main code yet to be made';
+  var code = '...\n';
   return code;
 };
