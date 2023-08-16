ccbf347a03c3f9f3ed2368a1a2ca1b0525219885
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-08-16 22:16:08 +0200

    adding process.env and basic starter code, completing docss

--- blocks/b4d/base_main_constructor.js
+++ blocks/b4d/base_main_constructor.js
@@ -29,6 +29,17 @@ javascript.javascriptGenerator.forBlock['base_main_constructor'] = function(bloc
   var statements_name = generator.statementToCode(block, 'NAME');
   var value_token = generator.valueToCode(block, 'TOKEN', javascript.Order.ATOMIC);
   // TODO: Assemble javascript into code variable.
-  var code = '...\n';
+  var code = `const { Client, Intents } = require("discord.js");
+  const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  });
+  
+  client.on("ready", () => {
+    console.log("I am ready!");
+  });
+  
+  ${statements_name}
+  
+  client.login("${value_token}");`;
   return code;
 };
---
+++ blocks/b4d/base_process_env.js
@@ -0,0 +1,29 @@
+const ProcessEnv = "base_process_env";
+ const ProcessEnvData = {
+    "message0": "process.env %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "CODE",
+      "check": [ "Number", "String" ]
+    }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": '#43A6C6',
+  "tooltip": "",
+  "helpUrl": ""
+ };
+Blockly.Blocks[ProcessEnv] = {
+    init: function() {
+        this.jsonInit(ProcessEnvData);
+    }
+};
+
+javascript.javascriptGenerator.forBlock['base_process_env'] = function(block, generator) {
+    var value_code = generator.valueToCode(block, 'CODE', javascript.Order.ATOMIC);
+    // TODO: Assemble javascript into code variable.
+    var code = `process.env.[string(${value_code})]`;
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, javascript.Order.ATOMIC];
+  };
