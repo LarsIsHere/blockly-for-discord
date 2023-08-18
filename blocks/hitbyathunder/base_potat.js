61c067f8f948cd483142632c68402dd9d889acb8
HitByaThunder <123588481+HitByaThunder@users.noreply.github.com>
2023-08-19 00:12:24 +0200

    Update base_potat.js

--- blocks/hitbyathunder/base_potat.js
+++ blocks/hitbyathunder/base_potat.js
@@ -22,9 +22,9 @@ Blockly.Blocks[potat] = {
 };
 
 javascript.javascriptGenerator.forBlock['base_process_env'] = function(block, generator) {
-    var value_code = generator.valueToCode(block, 'CODE', javascript.Order.ATOMIC);
+    var value_code = generator.valueToCode(block, 'POTAT_CODE', javascript.Order.ATOMIC);
     // TODO: Assemble javascript into code variable.
-    var code = `print("potato");`;
+    var potat_code = `print("potato");`;
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, javascript.Order.ATOMIC];
   };
