259006e4f42684c0ece99ea236fc6c4492a5276c
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-08-19 23:02:51 +0200

    woooo long boi. aka intents dropdown

---
+++ blocks/b4d/base_gateways.js
@@ -0,0 +1,113 @@
+const gatewaysName = "base_gateways";
+ const gatewaysData = {
+    "message0": "%1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "NAME",
+        "options": [
+          [
+            "Message Content",
+            "MessageContent"
+          ],
+          [
+            "Servers (Guilds)",
+            "Guilds"
+          ],
+          [
+            "Webhooks",
+            "GuildWebhooks"
+          ],
+          [
+            "Voice States",
+            "GuildVoiceStates"
+          ],
+          [
+            "Scheduled Events",
+            "GuildScheduledEvents"
+          ],
+          [
+            "Presence",
+            "GuildPresences"
+          ],
+          [
+            "Moderation",
+            "GuildModeration"
+          ],
+          [
+            "Guild Messages",
+            "GuildMessages"
+          ],
+          [
+            "Message Typing",
+            "GuildMessageTyping"
+          ],
+          [
+            "Message Reactions",
+            "GuildMessageReactions"
+          ],
+          [
+            "Members",
+            "GuildMembers"
+          ],
+          [
+            "Invites",
+            "GuildInvites"
+          ],
+          [
+            "Integrations",
+            "GuildIntegrations"
+          ],
+          [
+            "Emojis and Stickers",
+            "GuildEmojisAndStickers"
+          ],
+          [
+            "Bans",
+            "GuildBans"
+          ],
+          [
+            "Direct Messages",
+            "DirectMessages"
+          ],
+          [
+            "Direct Message Typing",
+            "DirectMessageTyping"
+          ],
+          [
+            "Direct Message Reactions",
+            "DirectMessageReactions"
+          ],
+          [
+            "Auto Moderation Execution",
+            "AutoModerationExecution"
+          ],
+          [
+            "Auto Moderation Configuration",
+            "AutoModerationConfiguration"
+          ],
+          [
+            "All",
+            "all"
+          ]
+        ]
+      }
+    ],
+    "output": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+ };
+Blockly.Blocks[gatewaysName] = {
+    init: function() {
+        this.jsonInit(gatewaysData);
+    }
+};
+
+javascript.javascriptGenerator.forBlock['base_gateways'] = function(block, generator) {
+    var dropdown_name = block.getFieldValue('NAME');
+    // TODO: Assemble javascript into code variable.
+    var code = '...';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, javascript.ORDER.NONE];
+  };
