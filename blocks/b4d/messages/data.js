3fc37f6b9516c3e5228d3560e2b486e1b740bd7b
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-08-21 15:35:11 +0200

    Update data.js

--- blocks/b4d/messages/data.js
+++ blocks/b4d/messages/data.js
@@ -1,1 +1,51 @@
+const qName = 'message_data';
 
+const qData = {
+    "message0": "message %1",
+    "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "author",
+          ".author"
+        ],
+        [
+          "content",
+          ".content"
+        ],
+        [
+          "channel id",
+          ".channelId"
+        ],
+        [
+          "id",
+          ".id"
+        ],
+        [
+          "message",
+          "OPTIONNAME"
+        ],
+        [
+          "server id",
+          ".guildId"
+        ]
+      ]
+    }
+  ],
+    "colour": "38b5cf",
+    "tooltip": 'Message author',
+    "helpUrl": '',
+    "output": 'user',
+};
+
+Blockly.Blocks[qName] = {
+    init: function() {
+        this.jsonInit(qData);
+    }
+};
+javascript.javascriptGenerator.forBlock[qName] = function() {
+  var dropdown_name = block.getFieldValue('NAME');
+  return [`message${dropdown_name}`, javascript.Order.NONE];
+};
