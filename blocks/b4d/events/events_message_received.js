f6c199bfe4e1ee0bb418509a3c24817ded45e15c
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-08-21 11:39:50 +0200

    2 events

---
+++ blocks/b4d/events/events_message_received.js
@@ -0,0 +1,33 @@
+const EventsMessageName = "events_message_received";
+ const EventsMessageData = {
+    "message0": "When a message is sent %1 %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_statement",
+        "name": "Event"
+      }
+    ],
+    "colour": "#ffa200",
+    "tooltip": "",
+    "helpUrl": ""
+ };
+Blockly.Blocks[EventsMessageName] = {
+    init: function() {
+        this.jsonInit(EventsMessageData);
+    }
+};
+
+javascript.javascriptGenerator.forBlock['events_message_received'] = function(block, generator) {
+    var statements_event = generator.statementToCode(block, 'Event');
+    // TODO: Assemble javascript into code variable.
+    var code = `client.on("message", (message) => {
+        ${statements_event}
+    });\n`;
+    return code;
+  };
+
+
+
