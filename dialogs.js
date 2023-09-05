55e8d8dd10c9f6e72d0e7a82bc52d926298ddd15
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-09-05 11:06:40 +0200

    Update dialogs.js

--- dialogs.js
+++ dialogs.js
@@ -6,7 +6,18 @@ function livecollabOpen() {
     document.getElementById("settingsdialog").close();
   }
 
+  function setDefault(dropdown, value) {
+    for(var i, j = 0; i = dropdown.options[j]; j++) {
+      if(i.value == value) {
+        dropdown.selectedIndex = j;
+        break;
+      }
+    }
+  }
+
   function settingsOpen() {
+    setDefault(document.getElementById('themedrop'), localStorage.getItem("theme"));
+    setDefault(document.getElementById('renderdrop'), localStorage.getItem("renderer"));
     document.getElementById("settings").showModal();
   }
   function settingsClose() {
