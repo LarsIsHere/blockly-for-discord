3bcf6973d5ab189e65ede1bcb1a575a1e14ff991
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-09-05 10:50:10 +0200

    e

---
+++ dialogs.js
@@ -0,0 +1,14 @@
+function livecollabOpen() {
+    document.getElementById("settingsdialog").showModal();
+  }
+
+  function livecollabClose() {
+    document.getElementById("settingsdialog").close();
+  }
+
+  function settingsOpen() {
+    document.getElementById("settings").showModal();
+  }
+  function settingsClose() {
+    document.getElementById("settings").close();
+  }
