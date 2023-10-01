8193f76874843581ac0ebb8a5f781e26ac4df24c
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-10-01 18:06:04 +0200

    Update base_advanced_main_contructor.js

--- blocks/b4d/base_advanced_main_contructor.js
+++ blocks/b4d/base_advanced_main_contructor.js
@@ -1,6 +1,6 @@
 const ConstructorName = "base_advanced_main_constructor";
  const ConstructorData = {
-    "message0": "Start bot with token %1 Listen to Events %2 Mobile? %3",
+    "message0": "Start bot with token %1 Listen to Events %2 Mobile? %3 %4 Using Libraries %5 %6",
     "args0": [
       {
         "type": "input_value",
@@ -8,13 +8,22 @@ const ConstructorName = "base_advanced_main_constructor";
       },
       {
         "type": "input_value",
-        "name": "intents",
-        "check": "Array"
+        "name": "intents"
       },
       {
         "type": "field_checkbox",
         "name": "mobile",
-        "checked": false
+        "checked": true
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_statement",
+        "name": "libraries"
       }
     ],
     "inputsInline": false,
@@ -29,9 +38,10 @@ javascript.javascriptGenerator.forBlock['base_advanced_main_constructor'] = func
   var value_token = generator.valueToCode(block, 'token', javascript.Order.ATOMIC);
   var value_intents = generator.valueToCode(block, 'intents', javascript.Order.ATOMIC);
   var value_mobile = block.getFieldValue('mobile') === 'TRUE';
+  var statements_libraries = generator.statementToCode(block, 'libraries');
   // TODO: Assemble javascript into code variable.
   var code = `const { Client, Events, IntentsBitField } = require('discord.js');
-
+  ${statements_libraries}
 
   const client = new Client(
     {
