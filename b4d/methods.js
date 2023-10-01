e6c37ba7a6256e4c249239d9ddb7052d6590c3e9
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-10-01 18:31:00 +0200

    Update methods.js

--- b4d/methods.js
+++ b4d/methods.js
@@ -55,7 +55,7 @@ b4d.success = function(message, duration, pos1="bottom", pos2="center") {
     document.getElementById(id).showModal();
   }
 
-  b4d.extension.load = function(blocks, name, color, category, dep){
+  b4d.extension.load = function(blocks, name, color, category, dep, customxml){
     var disable = document.getElementById(name);
 disable.disabled = true;
 disable.innerText = "Already in the Toolbox";
@@ -88,7 +88,7 @@ disable.innerText = "Already in the Toolbox";
       }
     });
     let newXML = '<category name="' + category + '" colour="' + color + '">';
-let completeXML = "<xml>"+document.getElementById("toolbox-categories").innerHTML.replaceAll("\n","")+newXML+xmlresult+"</category></xml>";
+let completeXML = "<xml>"+document.getElementById("toolbox-categories").innerHTML.replaceAll("\n","")+newXML+xmlresult+customxml+"</category></xml>";
 document.getElementById("toolbox-categories").innerHTML = completeXML.replace("<xml>","").replace("</xml>","")
 workspace.updateToolbox(Blockly.utils.xml.textToDom(completeXML));
 } catch (error) {
