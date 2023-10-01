273a244bebce2fbb6a2f9ea765522977961f22c3
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-10-01 19:24:55 +0200

    test2

--- b4d/methods.js
+++ b4d/methods.js
@@ -80,9 +80,9 @@ disable.innerText = "Already in the Toolbox";
       if (!(xml == "")) {
 
         let xmlback = xml.replace(/\*/g, '"');
-        let completeXML = "<xml>"+document.getElementById("toolbox-categories").innerHTML.replaceAll("\n","")+xmlback+"</xml>";
-        document.getElementById("toolbox-categories").innerHTML = completeXML.replace("<xml>","").replace("</xml>","")
-        workspace.updateToolbox(Blockly.utils.xml.textToDom(completeXML));
+        let completeXML2 = "<xml>"+document.getElementById("toolbox-categories").innerHTML.replaceAll("\n","")+xmlback+"</xml>";
+        document.getElementById("toolbox-categories").innerHTML = completeXML2.replace("<xml>","").replace("</xml>","")
+        workspace.updateToolbox(Blockly.utils.xml.textToDom(completeXML2));
 
       } else {
 
