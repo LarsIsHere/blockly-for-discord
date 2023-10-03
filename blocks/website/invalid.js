f9a30d6bb26faac7ae4be971ddf91d8ad987bb8f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-10-03 19:23:15 +0200

    Create invalid.js

---
+++ blocks/website/invalid.js
@@ -0,0 +1,25 @@
+const website_invalid_name = 'website_invalid';
+
+const website_invalid_data = {
+  "message0": "On non handeled request do %1",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "NAME"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 195,
+  "tooltip": "On non handeled request do something",
+  "helpUrl": ""
+}
+
+b4d.register(website_invalid_name, website_invalid_data)
+
+javascript.javascriptGenerator.forBlock[website_invalid_name] = function(block, generator) {
+  var statements_name = generator.statementToCode(block, 'NAME');
+  return `express_app.use(function(req, res) {
+  ${statements_name}
+});\n`
+};
