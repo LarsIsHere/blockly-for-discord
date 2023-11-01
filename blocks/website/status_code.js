c86bd6120ca6908b64d13ba47a56fe90ce3488c4
inventionpro <109528211+inventionpro@users.noreply.github.com>
2023-11-01 11:18:08 +0100

    Create status_code.js

---
+++ blocks/website/status_code.js
@@ -0,0 +1,24 @@
+const website_send_text_name = 'website_status_code';
+
+const website_status_code_data = {
+  "message0": "Set status code to %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "text",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 195,
+  "tooltip": "Respond with text",
+  "helpUrl": ""
+}
+
+b4d.register(website_status_code_name, website_status_code_data)
+
+javascript.javascriptGenerator.forBlock[website_status_code_name] = function(block, generator) {
+  var value_text = generator.valueToCode(block, 'text', javascript.Order.ATOMIC);
+  return `res.status(Number(${value_text}));\n`
+};
