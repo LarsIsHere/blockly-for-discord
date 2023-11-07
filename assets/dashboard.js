aa2c48f70f8ba3d8d11bc3283bfcefae55dbe6bf
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-11-07 20:54:55 +0100

    Update dashboard.js

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -16,13 +16,14 @@ const router = {
     
     if (window.location.pathname === '/dashboard' || window.location.pathname === '/dashboard/') {
       LoadContent(router["/"].dest);
-      
-    } else if (router[first_path]) {
-        const parts = window.location.pathname.split('/dashboard/')[1];
-        const first_path = parts.split('/')[0];
-        LoadContent(router[first_path].dest);
     } else {
-      LoadContent(router["404"].dest);
+      const parts = window.location.pathname.split('/dashboard/')[1];
+      const first_path = parts.split('/')[0];
+      if (router[first_path]) {
+        LoadContent(router[first_path].dest);
+      } else {
+        LoadContent(router["404"].dest);
+      }
     }
   });
   
