778df6faf4c853250df515c42e94825f5efdcd73
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-11-07 17:51:49 +0100

    index.js router

---
+++ index.js
@@ -0,0 +1,38 @@
+const router = {
+    "/": {
+      "dest" : "dashboard"
+    },
+    "browse": {
+      "dest" : "browse_projects"
+    },
+    "404": {
+      "dest" : "404"
+    }
+  };
+  
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    
+    const parts = windows.location.pathname.split('/dashboard/')[1];
+    const first_path = parts.split('/')[0];
+    if (window.location.pathname === '/dashboard' || window.location.pathname === '/dashboard/') {
+      LoadContent(router["/"].dest);
+      
+    } else if (router[first_path]) {
+        LoadContent(router[first_path].dest);
+    } else {
+      LoadContent(router["404"].dest);
+    }
+  });
+  
+  function LoadContent (item) {
+    switch(item) {
+      case 'dashboard':
+        //dashboard code stuff blabla
+        break;
+      case 'browse':
+        //browse code stuff blabla
+        break;
+      
+    }
+  }
