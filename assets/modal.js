bf57de6d7ce5a46247d63e8c40dff8ed59bdca9a
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-11-19 21:14:37 +0100

    thgang

---
+++ assets/modal.js
@@ -0,0 +1,17 @@
+import Toastify from 'toastify-js';
+
+
+Toastify({
+    text: "This is a toast",
+    duration: 12000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
