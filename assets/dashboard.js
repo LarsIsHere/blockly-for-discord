5a1c1d56f637076fb1f4b54906f24d5231331b4f
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2023-12-02 22:40:19 +0100

    dash

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -108,42 +108,20 @@ const router = {
         break;
 
         case 'dashboard':
-          document.title = "B4D │ My Projects";
-          const dashboardHTML = `             <div class="content-header" id="content-header">
+          document.title = "B4D │ Dashboard";
+          const dashboardHTML = `  <script>
+          LoadPreloader ("content-dash", 5000, '<div style="text-align:center"><h1 class="shortcuttext">Shortcuts</h1><h3 class="shortcuttext">Shortcuts to get you started quickly</h3></div><div class="quickmenu"><div style="display:flex;flex-direction:row;justify-content:center;height:100%"><div class="quickitem quickleft"><div><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></div><div><h3>Create Project</h3><p>Create a new Project to get started</p></div></div><div class="quickitem quickright"><div><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q146 0 255.5 91.5T872-559h-82q-19-73-68.5-130.5T600-776v16q0 33-23.5 56.5T520-680h-80v80q0 17-11.5 28.5T400-560h-80v80h80v120h-40L168-552q-3 18-5.5 36t-2.5 36q0 131 92 225t228 95v80Zm364-20L716-228q-21 12-45 20t-51 8q-75 0-127.5-52.5T440-380q0-75 52.5-127.5T620-560q75 0 127.5 52.5T800-380q0 27-8 51t-20 45l128 128-56 56ZM620-280q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29Z"/></svg></div><div><h3>Find Projects</h3><p>Find projects other people have created</p></div></div></div><div><div class="alert alert-primary" role="alert" style="margin-bottom:0" data-bs-theme="dark">Having trouble finding your way? Join our<a href="https://discord.gg/77XrD6UTB3" target="_blank" class="alert-link">discord server</a>to get help or<strong>start the tour</strong>.</div></div></div>');
+        </script>
+        <div class="content-header" id="content-header">
           <h2>Dashboard</h2>
           <p>All the things you want to do in one place</p>
           <div class="content-seperator"></div>
           
         </div>
-        <div class="content-dash">
-          <div style="text-align: center;">
-            <h1 class="shortcuttext">Shortcuts</h1>
-            <h3 class="shortcuttext">Shortcuts to get you started quickly</h3>
-          </div>
-          <div class="quickmenu">
-            <div style="display: flex; flex-direction: row; justify-content: center; height: 100%;">
-              <div class="quickitem quickleft">
-                <div><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></div>
-                <div>
-                  <h3>Create Project</h3>
-                  <p>Create a new Project to get started</p>
-                </div>
-              </div>
-              <div class="quickitem quickright">
-                <div><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q146 0 255.5 91.5T872-559h-82q-19-73-68.5-130.5T600-776v16q0 33-23.5 56.5T520-680h-80v80q0 17-11.5 28.5T400-560h-80v80h80v120h-40L168-552q-3 18-5.5 36t-2.5 36q0 131 92 225t228 95v80Zm364-20L716-228q-21 12-45 20t-51 8q-75 0-127.5-52.5T440-380q0-75 52.5-127.5T620-560q75 0 127.5 52.5T800-380q0 27-8 51t-20 45l128 128-56 56ZM620-280q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29Z"/></svg></div>
-                <div>
-                  <h3>Find Projects</h3>
-                  <p>Find projects other people have created</p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="alert alert-primary" role="alert" style="margin-bottom: 0px;" data-bs-theme="dark">Having trouble finding your way? Join our <a href="https://discord.gg/77XrD6UTB3" target="_blank" class="alert-link">discord server</a> to get help or <strong>start the tour</strong>.</div>
-          </div>
-          </div>
+        <div class="content-dash" id="content-dash">
+
         </div>
-          </div>
-        </div>`;
+        ` ;
 
         parent.innerHTML = dashboardHTML;
         CurrentPage = "dashboard";
@@ -240,3 +218,19 @@ async function hideDialog() {
     dialog.style.display = 'none';
   }
 }
+
+
+function LoadPreloader (div, time=0, inject=' ') {
+  const ParentElement = document.getElementById(div);
+
+  ParentElement.innerHTML = `<div class="spinner-border text-success" role="status"><span class="visually-hidden">Loading...</span></div>`;
+  if (!time === 0) {
+    setTimeout(() => {
+      ParentElement.innerHTML = inject;
+    }, time);
+
+  }
+
+}
+
+
